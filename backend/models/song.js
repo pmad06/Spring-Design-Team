e7b288797b3a2fe0edcb2f906ce701f20c7b29bc
pmad06 <p.madishetty@ufl.edu>
2026-03-26 11:35:38 -0400

    edited song schema

--- backend/models/song.js
+++ backend/models/song.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const songSchema = new mongoose.Schem({
+const songSchema = new mongoose.Schema({
     title: { type: String, required: true },
     artist: { type: String, required: true },
     lyrics: { type: String, default: '' },
